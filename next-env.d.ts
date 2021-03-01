1c7f5686aad15c96680b36ea69ab4480edcc39d2
nicholas-roether <nicholas.roether@t-online.de>
2021-03-01 17:18:15 +0100

    Add environment variables

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,15 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			NEXTAUTH_URL: string;
+			AUTH_DATABASE_URL: string;
+			GOOGLE_CLIENT_ID: string;
+			GOOGLE_CLIENT_SECRET: string;
+		}
+	}
+}
+
+export {}
