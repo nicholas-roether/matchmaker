038b47ab944eb497f35dcbc3ccdf200cb575c783
nicholas-roether <nicholas.roether@t-online.de>
2021-03-01 17:19:04 +0100

    Add temporary login solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import { Box, Container } from '@material-ui/core';
+import { Box, Button, Container } from '@material-ui/core';
+import { signIn, useSession } from 'next-auth/client';
 import Head from 'next/head'
 import React from 'react';
 
 const Home = () => {
+  const [session, loading] = useSession();
   return (
     <>
       <Head>
@@ -10,7 +12,15 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container maxWidth="lg" component="main">
-        <Box mt={3} textAlign="center">Page under construction</Box>
+        <Box mt={3} textAlign="center">
+          Page under construction
+          <br />
+          {
+            loading ? "Loading..." :
+            session.user ? JSON.stringify(session.user) :
+            <Button variant="contained" color="primary" onClick={() => signIn()}>Login</Button>
+          }
+        </Box>
       </Container>
     </>
   )
