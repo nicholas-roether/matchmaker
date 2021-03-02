26d13e58e0a9b17cbb72d90de886acf10527e43d
nicholas-roether <nicholas.roether@t-online.de>
2021-03-02 01:05:37 +0100

    Replace spaces with tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,31 @@ import Head from 'next/head'
 import React from 'react';
 
 const Home = () => {
-  const [session, loading] = useSession();
-  if(loading) return null;
-  return (
-    <>
-      <Head>
-        <title>Matchmaker | Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container maxWidth="lg" component="main">
-        <Box mt={3} textAlign="center">
-          Page under construction
-          <br />
-          {
-            session ? (
-              <>
-                <code>{JSON.stringify(session.user)}</code>
-                <Button variant="contained" color="primary" onClick={() => signOut()}>Logout</Button>
-              </>
-            ) :
-            <Button variant="contained" color="primary" onClick={() => signIn()}>Login</Button>
-          }
-        </Box>
-      </Container>
-    </>
-  )
+	const [session, loading] = useSession();
+	if(loading) return null;
+	return (
+		<>
+			<Head>
+				<title>Matchmaker | Home</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Container maxWidth="lg" component="main">
+				<Box mt={3} textAlign="center">
+					Page under construction
+					<br />
+					{
+						session ? (
+							<>
+								<code>{JSON.stringify(session.user)}</code>
+								<Button variant="contained" color="primary" onClick={() => signOut()}>Logout</Button>
+							</>
+						) :
+						<Button variant="contained" color="primary" onClick={() => signIn()}>Login</Button>
+					}
+				</Box>
+			</Container>
+		</>
+	)
 }
 
 export default Home;
