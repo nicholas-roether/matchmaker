4f733c2bd09fbeca5ce2eb5ee2b998202311eb77
nicholas-roether <nicholas.roether@t-online.de>
2021-03-02 20:29:27 +0100

    Make homepage responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,31 @@
-import { Box, Button, Container, Typography } from '@material-ui/core';
+import { Box, Button, Container, Typography, useMediaQuery, useTheme } from '@material-ui/core';
 import { signIn } from 'next-auth/client';
 import Head from 'next/head'
 import React from 'react';
 
 const Home = () => {
+	const theme = useTheme();
+	const small = useMediaQuery(theme.breakpoints.down("xs"));
 	return (
 		<>
 			<Head>
 				<title>Matchmaker | Home</title>
 			</Head>
 			<Container maxWidth="md">
-				<Box mt={8} textAlign="center" marginX="auto">
-					<Typography variant="h2" gutterBottom>
+				<Box mt={small ? 6 : 10} textAlign="center" marginX="auto">
+					<Typography variant={small ? "h3": "h2"} gutterBottom>
 						Organize tournaments,<br />
 						the easy way.
 					</Typography>
-					<Typography paragraph style={{fontSize: "1.7em"}}>
+					<Typography paragraph style={{fontSize: small ? "1.2em" : "1.7em"}}>
 						With Matchmaker, you can organize tournaments of any scale,
 						for free! Let us handle the organizational hassle while you
 						enjoy the competition.
 					</Typography>
 					<Box mt={3}>
-						<Button variant="contained" color="primary" size="large" onClick={() => signIn()}>Get Started</Button>
+						<Button variant="contained" color="primary" size={small ? "medium" : "large"} onClick={() => signIn()}>Get Started</Button>
 						<Box mr={1} display="inline" />
-						<Button variant="outlined" color="primary" size="large">Watch Tutorial</Button>
+						<Button variant="outlined" color="primary" size={small ? "medium" : "large"}>Watch Tutorial</Button>
 					</Box>
 				</Box>
 			</Container>
