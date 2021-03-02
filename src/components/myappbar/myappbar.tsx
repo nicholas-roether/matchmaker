6df1ffb4a8f5259110a2555e44eddd56a8dd1b3b
nicholas-roether <nicholas.roether@t-online.de>
2021-03-02 20:30:57 +0100

    Add menu compatibility and refactoring

--- src/components/myappbar/myappbar.tsx
+++ src/components/myappbar/myappbar.tsx
@@ -1,19 +1,25 @@
-import { AppBar, Box, IconButton, Toolbar, Typography, useMediaQuery, useTheme } from "@material-ui/core";
+import { AppBar, Box, IconButton, makeStyles, Toolbar, Typography, useMediaQuery, useTheme } from "@material-ui/core";
 import { MenuTwoTone as MenuIcon } from "@material-ui/icons";
 import React from "react";
 import UserDisplay from "./user_display";
 
-const MyAppBar = () => {
+
+interface MyAppBarProps {
+	onMenuToggle: () => void;
+}
+
+const MyAppBar = ({onMenuToggle}: MyAppBarProps) => {
 	const theme = useTheme();
-	const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+	const tiny = useMediaQuery(theme.breakpoints.down("sm"));
+
 	return (
 		<AppBar position="sticky">
 			<Toolbar>
-				<IconButton edge="start"><MenuIcon /></IconButton>
+				<IconButton edge="start" onClick={() => onMenuToggle()}><MenuIcon /></IconButton>
 				<Box mr={1} />
-				<Typography variant={mobile ? "h6" : "h5"}>Matchmaker</Typography>
+				<Typography variant={tiny ? "h6" : "h5"}>Matchmaker</Typography>
 				<Box flexGrow={1} />
-				<UserDisplay mobile={mobile} />
+				<UserDisplay tiny={tiny} />
 			</Toolbar>
 		</AppBar>
 	)
