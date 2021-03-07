a375b650969cd789157f381cadd776f4652ca56f
nicholas-roether <nicholas.roether@t-online.de>
2021-03-08 00:53:06 +0100

    Create Database class

---
+++ src/database/database.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { CompetitorSchema, TournamentSchema, UserSchema } from "./schema";
+
+type MongooseModel = mongoose.Model<mongoose.Document<any>>;
+
+interface Models {
+	User: MongooseModel
+	Competitor: MongooseModel,
+	Tournament: MongooseModel
+}
+
+class Database {
+	private static connectionString = process.env.DATABASE_URL;
+	private connection: mongoose.Connection;
+	public models: Models;
+
+	public async connect() {
+		this.connection = await mongoose.createConnection(Database.connectionString);
+		this.models = {
+			User: this.connection.model("user", UserSchema),
+			Competitor: this.connection.model("competitor", CompetitorSchema),
+			Tournament: this.connection.model("tournament", TournamentSchema)
+		}
+	}
+
+	public get connected() { return Boolean(this.connection); }
+}
