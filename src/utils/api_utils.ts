8799d941bef01ab9e7a3e982be1730d75df6238f
nicholas-roether <nicholas.roether@t-online.de>
2021-03-29 01:02:03 +0200

    Add a bunch of utility functions to ApiResponse

--- src/utils/api_utils.ts
+++ src/utils/api_utils.ts
@@ -9,6 +9,10 @@ class ApiResponse {
 		this.error = error ?? null;
 	}
 
+	public json() {
+		return JSON.stringify(this);
+	}
+
 	public static data(data: any) {
 		return new ApiResponse(data);
 	}
@@ -16,15 +20,34 @@ class ApiResponse {
 	public static error(message: string) {
 		return new ApiResponse(null, message);
 	}
-}
+
+	public static isApiResponse(data: any): data is ApiResponse {
+		return (!("error" in data) && "data" in data) || typeof data.error === "string";
+	}
+
+	public static fromJson(json: string) {
+		const data = JSON.parse(json);
+		return this.isApiResponse(data) ? data : null;
+	}
+ }
 
 function requireMethod(method: string, req: NextApiRequest, res: NextApiResponse) {
 	let valid = req.method == method;
-	if(!valid) res.status(405).end(ApiResponse.error("Invalid Method"));
+	if(!valid) res.status(405).end(ApiResponse.error("Invalid Method").json());
 	return valid;
+}
+
+function getValidJson(body: string, res: NextApiResponse): string {
+	try {
+		return JSON.parse(body);
+	} catch(e) {
+		res.status(400).end(ApiResponse.error(`Invalid Request: ${e}`).json());
+		return null;
+	}
 }
 
 export {
 	ApiResponse,
-	requireMethod
+	requireMethod,
+	getValidJson
 }
