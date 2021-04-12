2e32534db06927c0ace0a6a49e5df4585bacaeed
nicholas-roether <nicholas.roether@t-online.de>
2021-04-12 18:01:01 +0200

    Fix tournament deletion

--- pages/api/tournament/delete.ts
+++ pages/api/tournament/delete.ts
@@ -1,8 +1,11 @@
 import * as mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import Database from "../../../src/database/database";
+import { Competitor } from "../../../src/tournament/competitor";
+import TournamentController, { TournamentSyncType } from "../../../src/tournament/tournament_controller";
 import TournamentDBAdapter from "../../../src/tournament/tournament_db_adapter";
 import { ApiResponse, ArgumentSchema, extractData, requireMethod, requireOwnerPrivilege } from "../../../src/utils/api_utils";
+import { isValidEncodedObjectId } from "../../../src/utils/db_utils";
 
 interface TournamentDeleteOptions {
 	id: string;
@@ -11,7 +14,7 @@ interface TournamentDeleteOptions {
 const schema: ArgumentSchema = {
 	id: {
 		type: "string",
-		validate: (id) => mongoose.Types.ObjectId.isValid(id)
+		validate: (id) => isValidEncodedObjectId(id)
 	}
 }
 
@@ -22,12 +25,15 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
 	if(!data) return;
 
 	const db = new Database();
+	await db.connect();
+
 	const session = requireOwnerPrivilege(data.id, req, res, "Only the owner can delete a tournament", db);
 	if(!session) return;
 
 	try {
-		const [_, controller] = await TournamentDBAdapter.getTournament(data.id, db);
-		controller.delete();
+		const tournament = await TournamentDBAdapter.getTournament(data.id, db);
+		const controller = new TournamentController<Competitor>(tournament, TournamentSyncType.NONE, db);
+		await controller.delete();
 	} catch(e) {
 		return res.status(400).end(ApiResponse.error(`Failed to delete tournament: ${e}`).json());
 	}
